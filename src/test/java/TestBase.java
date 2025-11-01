084c0efb1b0893061c8b6ffe4485e13e18879d49
Tatiana Andreeva <andretat8@gmail.com>
2025-11-01 20:10:19 +0100

    Initial commit

--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -1,5 +1,7 @@
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class TestBase {
     @BeforeAll
@@ -8,5 +10,6 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
         Configuration.timeout = 5000;
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 }
